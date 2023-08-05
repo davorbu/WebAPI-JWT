dd43e4131c82500193dda890e9f06b16786777d6
Davor Burmudžija <davorburmudzija@gmail.com>
2023-08-05 16:38:32 +0200

    Add project files.

---
+++ WebAPI JWT/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI_JWT.Models
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
---
+++ WebAPI JWT/Models/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI_JWT.Models
+{
+    public class UserDto
+    {
+        public required string UserName { get; set; }
+
+        public required string  Password { get; set; }
+    }
+}
